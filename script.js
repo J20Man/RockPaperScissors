a40833d26cf73f51f23d6c38ce3dc17f32def72b
J20Man <124213140+J20Man@users.noreply.github.com>
2025-01-16 18:55:19 +0000

    Added game function Needs fixing

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
 var UserScore = 0;
 var CompScore = 0;
-
+// Rock = 1
+// Scissors = 2
+// Paper = 3
 function getComputerChoice() {
     const num = Math.random();
     var CompChoice;
     
     if (num <= 0.3) {
-        CompChoice = "Rock";
+        CompChoice = 1;
     } else if (num > 0.3 && num <= 0.6){
-        CompChoice = "Scissors";
+        CompChoice = 2;
     } else {
-        CompChoice = "Paper";
+        CompChoice = 3;
     }
 
-    console.log(CompChoice);
+    return CompChoice;
 }
 
 function getUserChoice() {
-    var UserChoice = prompt("Please enter you're choice ")
-    console.log(UserChoice)
+    var UserChoice = prompt("Please enter your choice ");
+    return UserChoice;
 }
 
-getComputerChoice();
-getUserChoice();
+function playRound(User, Comp) {
+      var calculator = 0;
+    const humanSelction = getUserChoice().toLowerCase;
+    const ComputerSelection = getComputerChoice();
+
+    if (User == "rock") {
+        calculator = 1;
+    } else if (User == "scissors") {
+        calculator = 2;
+    } else {
+        calculator = 3;
+    }
+
+    if (calculator == 1 && Comp == 2){
+        console.log("Congradulations, you win!");
+        UserScore = UserScore + 1;
+    } else if (calculator == 2 && Comp == 3){
+        console.log("Congradulations, you win!");
+        UserScore = UserScore + 1;
+    } else if (calculator == 3 && Comp == 1){
+        console.log("Congradulations, you win!");
+        UserScore = UserScore + 1;
+
+    } else if (calculator == 2 && Comp == 1){
+        console.log("I'm sorry, you lose");
+        CompScore = CompScore + 1;
+    } else if (calculator == 3 && Comp == 2){
+        console.log("I'm sorry, you lose");
+        CompScore = CompScore + 1;
+    } else if (calculator == 1 && Comp == 3){
+        console.log("I'm sorry, you lose");
+        CompScore = CompScore + 1;
+    } else {
+        console.log("I'm sorry it's a tie!")
+    }
+      
+}
+
+playRound()
